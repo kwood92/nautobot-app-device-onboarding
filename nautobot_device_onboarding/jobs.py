0fec757286595c99ae05e27717c4ea9f481d10b5
Kaigan Woodford <kwoodford@summitinternet.com.au>
2024-10-04 14:37:33 +1000

    More debugging

--- nautobot_device_onboarding/jobs.py
+++ nautobot_device_onboarding/jobs.py
@@ -461,7 +461,7 @@ class SSOTSyncDevices(DataSource):  # pylint: disable=too-many-instance-attribut
                 for ip_address in self.processed_csv_data:
                     self.ip_addresses.append(ip_address)
                 # prepare the task_kwargs needed by the CommandGetterDO job
-                self.job_result.task_kwargs = {"debug": debug, "csv_file": self.task_kwargs_csv_data}
+                self.job_result.task_kwargs = {"debug": debug, "csv_file": self.task_kwargs_csv_data, "set_send_command_timing": set_send_command_timing}
             else:
                 raise ValidationError(message="CSV check failed. No devices will be synced.")
 
