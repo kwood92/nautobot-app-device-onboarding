b91f3b7ee767f69dfc85e4b162cd17dc4ec8d012
Kaigan Woodford <kwoodford@summitinternet.com.au>
2024-10-11 11:32:34 +1000

    Update sync_network_data_adapters.py

--- nautobot_device_onboarding/diffsync/adapters/sync_network_data_adapters.py
+++ nautobot_device_onboarding/diffsync/adapters/sync_network_data_adapters.py
@@ -121,7 +121,7 @@ class SyncNetworkDataNautobotAdapter(FilteredNautobotAdapter):
 
         Only Vlans that were returned by the CommandGetter job should be synced.
         """
-        for vlan in VLAN.objects.filter(locations__in=self.job.devices_to_load):
+        for vlan in VLAN.objects.filter(locations__in=self.job.location):
             network_vlan = self.vlan(
                 adapter=self,
                 name=vlan.name,
